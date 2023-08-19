911a1384a7742c30ed2a9f98974d8758b6224daf
cryptbrn <cryptbrn@gmail.com>
2023-08-19 20:17:37 +0700

    setup tailwind and clear home view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,19 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="container max-w-fit mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10">
+    <p class="text-xl text-gray-700 font-bold mb-5 text-center">
+      Hello! I'm Muhammad Reyhan
+    </p>
+    <p class="text-gray-500 text-md text-center">
+      Backend Engineer
+    </p>
+    <p class="text-gray-500 text-sm text-center">
+      Currently work for Blibli.com
+    </p>
+    <p class="text-gray-500 text-sm text-center">
+      Passionate Backend Engineer with a Dash of Mobile Experience.
+    </p>
+  </div>
 </template>
